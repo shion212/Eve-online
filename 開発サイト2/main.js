93f4ee568ddb1e96739ffad61752f59685d336fb
shion212 <68310617+shion212@users.noreply.github.com>
2020-12-13 13:52:24 +0900

    Eve online
    古着のファッションサイトです。

---
+++ 開発サイト2/main.js
@@ -0,0 +1,122 @@
+function toggleNav() {
+  var body = document.body;
+  var hamburger = document.getElementById('js-hamburger');
+  var blackBg = document.getElementById('js-black-bg');
+
+  hamburger.addEventListener('click', function() {
+    body.classList.toggle('nav-open');
+  });
+  blackBg.addEventListener('click', function() {
+    body.classList.remove('nav-open');
+  });
+}
+toggleNav();
+
+
+
+
+
+//ここからjquery
+$(function() {
+
+  $('#login-show').click(function() {
+    $('#login-modal').fadeIn();
+  });
+
+  $('.signup-show').click(function() {
+    $('#signup-modal').fadeIn();
+  });
+
+  $('.close-modal').click(function() {
+    $('#login-modal').fadeOut();
+    $('#signup-modal').fadeOut();
+  });
+
+  $('.lesson-hover').hover(
+    function() {
+      $(this).find('.text-contents').addClass('text-active');
+    },
+    function() {
+      $(this).find('.text-contents').removeClass('text-active');
+    }
+  );
+
+  // FAQのアコーディオン
+  $('.faq-list-item').click(function() {
+    var $answer = $(this).find('.answer');
+    if($answer.hasClass('open')) {
+      $answer.removeClass('open');
+      // slideUpメソッドを用いて、$answerを隠してください
+      $answer.slideUp();
+
+      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+      $(this).find('span').text('+');
+
+    } else {
+      $answer.addClass('open');
+      // slideDownメソッドを用いて、$answerを表示してください
+      $answer.slideDown();
+      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+      $(this).find('span').text('-');
+
+    }
+  });
+});
+
+
+
+//jounal
+$(function() {
+
+  function toggleChangeBtn() {
+    var slideIndex = $('.slide').index($('.active'));
+    $('.change-btn').show();
+    if (slideIndex == 0) {
+      $('.prev-btn').hide();
+    // 「3」の部分を、lengthメソッドを用いて書き換えてください
+    } else if (slideIndex == $('.slide').length - 1) {
+      $('.next-btn').hide();
+    }
+  }
+
+  $('.index-btn').click(function() {
+    $('.active').removeClass('active');
+    var clickedIndex = $('.index-btn').index($(this));
+    $('.slide').eq(clickedIndex).addClass('active');
+    toggleChangeBtn();
+
+  });
+
+  $('.change-btn').click(function() {
+    var $displaySlide = $('.active');
+    $displaySlide.removeClass('active');
+    if ($(this).hasClass('next-btn')) {
+      $displaySlide.next().addClass('active');
+    } else {
+      $displaySlide.prev().addClass('active');
+    }
+    toggleChangeBtn();
+
+  });
+});
+
+
+
+
+
+  // SNSボタン
+  $('.social-icon').hover(
+    function(){
+      $(this).children('span').animate({
+        'font-size':'30px'
+      }, 300);
+    },
+    function(){
+      $(this).children('span').animate({
+        'font-size':'24px'
+      }, 300);
+    }
+  );
+
+
+
